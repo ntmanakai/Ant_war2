adbb75ecf9064a406439aaace2d24eaf268ace91
Florian MARTIN <florian.martin@imerir.com>
2022-04-25 15:45:52 +0200

    ADD map

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[][] matrice = new int[500][500];
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                matrice[i][j] = (int) (Math.random() * 16);
+                System.out.print(matrice[i][j]);
+            }
+            System.out.println();
+        }
+
+
     }
 }
