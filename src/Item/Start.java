c27681515fe2cf82358f956ae7b6530e4060ce70
Florian MARTIN <florian.martin@imerir.com>
2022-04-27 10:58:58 +0200

    ADD Start

---
+++ src/Item/Start.java
@@ -0,0 +1,22 @@
+package Item;
+
+public abstract class Start {
+
+    protected int position_X;
+    protected int position_Y;
+
+    public Start()
+    {
+
+    }
+
+    public int GetPositionX()
+    {
+        return this.position_X;
+    }
+
+    public int GetPositionY()
+    {
+        return this.position_Y;
+    }
+}
