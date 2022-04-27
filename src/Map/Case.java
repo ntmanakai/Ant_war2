66ebd23ceb5bbd3888cb61959a6bb9d13ae80945
Florian MARTIN <florian.martin@imerir.com>
2022-04-27 10:36:30 +0200

    ADD Attributes and Constructors

--- src/Map/Case.java
+++ src/Map/Case.java
@@ -1,5 +1,31 @@
 package Map;
 
-public class Case {
+import Fight.Fighters;
+
+public class Case implements Fighters{
+
+    /**************************************--Attributes--********************************************/
+
+    private boolean isAntHill;
+    private int position_X;
+    private int position_y;
+    private ArrayList<Fighters> FightersList;
+    private Food aFood;
+    private boolean aFight;
+
+    /**************************************--Constructors--******************************************/
+
+    public Case(int x , int y)
+    {
+        this.FightersList = new ArrayList<Fighters>();
+        this.position_X = x;
+        this.position_y = y;
+        this.isAntHill = false;
+        this.aFood = null;
+        this.aFight = false;
+    }
+
+    }
+
 
 }
